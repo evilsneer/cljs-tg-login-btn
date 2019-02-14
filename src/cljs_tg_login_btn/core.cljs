b5f4200162633e020569f087dd526420ac06b71a
vshishkov <vshishkov@amberdata.ru>
2019-02-14 16:30:37 +0300

    fix onload button script

--- src/cljs_tg_login_btn/core.cljs
+++ src/cljs_tg_login_btn/core.cljs
@@ -10,27 +10,43 @@
      :data-telegram-login "emptyonedishauth_bot"
      :data-size           "medium"
      :data-userpic        "false"
-     :data-onauth         (str callback-name "(user)")
-                          :data-radius         "5"
-                          :parent-sel          :#tg-login-fieldset}))
+     :data-onauth         (str callback-name "(user)")      ;"lovesexface.telegram_auth.onTelegramAuth(user)"
+     :data-radius         "5"
+     :parent-sel          :#tg-login-fieldset}
+    )
+  )
 
 (defn telegram-btn [is-active-atom callback-name]
-  (let [observer (js/MutationObserver. (fn [mutations]
+  (let [onload #(reset! is-active-atom true)
+        observer (js/MutationObserver. (fn [mutations]
                                          (doseq [mutation mutations]
                                            (case (aget mutation "type")
-                                             "childList" (do
-                                                           (println "child added")
-                                                           (js/console.log mutation)
-                                                           (reset! is-active-atom true))
-                                             :default (println "another mutation happened")))))]
+                                             "childList" (let [added-nodes (.. mutation -addedNodes)
+                                                               added-node (.item added-nodes 0)
+                                                               added-tagname (.-tagName added-node)]
+                                                           (case added-tagname
+                                                             "IFRAME" (do
+                                                                        (println "Iframe added")
+                                                                        (js/console.log added-node added-tagname)
+                                                                        ;(println "adding iframe load listener")
+                                                                        (.addEventListener added-node "load" onload)
+                                                                        ;(onload)
+                                                                        )
+                                                             ;(println "another tag added")
+                                                             ))
+                                             ;(println "another mutation happened")
+                                             ))))]
     (reagent.core/create-class {:component-did-mount    (fn []
                                                           (let [element (.getElementById js/document "tg-login-fieldset")
-                                                                attributes (js-obj "attributeFilter" ["class"] "childList" true)]
+                                                                attributes (js-obj "attributeFilter" ["class"] "childList" true "subtree" true)]
                                                             (.observe observer element attributes)
-                                                            (println "Tg Button Observer added"))
+                                                            ;(println "Tg Button Observer added")
+                                                            )
                                                           (load-telegram-btn! callback-name))
 
-                                :component-will-unmount (fn [] (.disconnect observer) (println "Tg Button Observer unmounted"))
+                                :component-will-unmount (fn [] (.disconnect observer)
+                                                          ;(println "Tg Button Observer unmounted")
+                                                          )
 
                                 :reagent-render         (fn []
                                                           [:div#tg-login-fieldset])})))
