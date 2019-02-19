169b3bbfdd837e6712cabef5643873eeca621c41
vshishkov <vshishkov@amberdata.ru>
2019-02-19 17:21:15 +0300

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-tg-login-btn "0.1.0-SNAPSHOT"
+(defproject cljs-tg-login-btn "0.1.0"
   :description "Telegram login button for ClojureScript Reagent users"
   :url "https://github.com/evilsneer/cljs-tg-login-btn"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -8,4 +8,16 @@
                   :scope "provided"]
                  [reagent "0.8.1"]
                  [cljs-add-script "0.1.0-SNAPSHOT"]]
-  :repl-options {:init-ns cljs-tg-login-btn.core})
+  :repl-options {:init-ns cljs-tg-login-btn.core}
+
+  ;:aliases {"test" ^{:doc "Clean and Test"}
+  ;                 ["do" "clean" ["with-profile" "dev" "doo" "phantom" "test" "once"]]}
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["deploy"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
